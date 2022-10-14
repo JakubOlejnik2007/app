32fedb56a738e6c68290174b33b8639e0774de96
Jakub Olejnik <jacobole2000@gmail.com>
2022-10-14 19:04:47 +0200

    Changes by X-COM1

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,23 @@
 <!-- Widok o aplikacji -->
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>O aplikacji</h1>
+    Aplikacja (xyz) służy do zgłaszania problemów, takich jak popsuta lampa
+    uliczna czy zaśmiecenie na danym terenie.<br />
+    Użycie aplikacji jest bardzo proste i trafia prosto do znajdującego się w
+    twojej okolicy urzędu.<br />
+    Dzięki (xyz) zgłaszanie problemów jest prostsze niż kiedykolwiek.<br />
+    Aplikacja umożliwia również monitorowanie twojego zgłoszenia, czy zostało
+    ono przyjęte.<br />
+    <h2>Instrukcja obsługi aplikacji.</h2>
+    <h3>1. Logowanie do aplikacji</h3>
+    By utworzyć konto należy wprowadzić e-mail, Imię, Nazwisko oraz założyć
+    hasło do konta.<br />
+    <h3>2. Tworzenie zgłoszeń.</h3>
+    Aby utworzyć zgłoszenie należy przejść do zakładki Zgłoszenia i wypełnić
+    formularz zgłoszenia, dołączając tytuł zgłoszenia, opis i jeśli potrzebne,
+    zdjęcie.<br />
+    Wsićnij "zgłoś" i już gotowe! Możesz teraz monitorować swoje zgłoszenie i
+    czy zostało one przyjęcie przez pobliski urząd.<br />
   </div>
 </template>
